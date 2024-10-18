5a3af715c9682474b492d04b23d116e793d8420d
Lee Priest <lee@leepriest.dev>
2024-10-18 17:05:05 +0100

    Export EventBus construct

--- src/exports/index.ts
+++ src/exports/index.ts
@@ -1,3 +1,4 @@
 export * from '../constructs/StepFunctions/ExpressStepFunction';
 export * from '../constructs/APIGateway/APIGateway';
 export * from '../constructs/LambdaFunction/TSLambdaFunction';
+export * from '../constructs/EventBridge/EventBus/EventBus';
