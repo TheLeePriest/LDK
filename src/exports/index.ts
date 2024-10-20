682e5aef016f8600ffa46ea1ebf25a53f8420f93
Lee Priest <lee@leepriest.dev>
2024-10-20 18:29:09 +0100

    Add export and bump version

--- src/exports/index.ts
+++ src/exports/index.ts
@@ -2,3 +2,4 @@ export * from '../constructs/StepFunctions/ExpressStepFunction';
 export * from '../constructs/APIGateway/APIGateway';
 export * from '../constructs/LambdaFunction/TSLambdaFunction';
 export * from '../constructs/EventBridge/EventBus/EventBus';
+export * from '../constructs/IAM/User';
