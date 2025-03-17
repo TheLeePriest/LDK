d0ac2325d819a2ebf59163e0c2d39eb9df9389bf
Lee Priest <lee@leepriest.dev>
2025-03-17 00:17:34 +0000

    Update dependencies, add version path and custom domain functionality

--- src/constructs/APIGateway/APIGateway.ts
+++ src/constructs/APIGateway/APIGateway.ts
@@ -1,11 +1,19 @@
 import { Construct } from 'constructs';
-import { ApiKey, RestApi, UsagePlan } from 'aws-cdk-lib/aws-apigateway';
+import {
+  ApiKey,
+  BasePathMapping,
+  DomainName,
+  Resource,
+  RestApi,
+  UsagePlan,
+} from 'aws-cdk-lib/aws-apigateway';
 import { ApiGatewayProps } from '../../types/APIGateway.type';
 
 export class APIGateway extends Construct {
   public readonly restAPI: RestApi;
   public readonly apiKey: ApiKey;
   public readonly usagePlan: UsagePlan;
+  public readonly versionPath?: Resource;
 
   constructor(scope: Construct, id: string, props: ApiGatewayProps) {
     super(scope, id);
@@ -17,6 +25,10 @@ export class APIGateway extends Construct {
       burstLimit = 1000,
       restApiProps,
       apiKeyRequired = true,
+      version,
+      customDomainName,
+      domainNameAliasTarget,
+      domainNameAliasHostedZoneId,
     } = props;
 
     this.restAPI = new RestApi(this, `${serviceName}-restAPI-${stage}`, {
@@ -49,5 +61,33 @@ export class APIGateway extends Construct {
     });
 
     this.usagePlan.addApiKey(this.apiKey);
+
+    if (customDomainName) {
+      if (!domainNameAliasTarget || !domainNameAliasHostedZoneId) {
+        throw new Error(
+          'domainNameAliasTarget and domainNameAliasHostedZoneId must be provided when customDomainName is provided'
+        );
+      }
+
+      const customDomain = DomainName.fromDomainNameAttributes(
+        this,
+        `${serviceName}-customDomain-${stage}`,
+        {
+          domainName: customDomainName,
+          domainNameAliasTarget,
+          domainNameAliasHostedZoneId,
+        }
+      );
+
+      new BasePathMapping(this, `${serviceName}-basePathMapping-${stage}`, {
+        domainName: customDomain,
+        restApi: this.restAPI,
+        stage: this.restAPI.deploymentStage,
+      });
+    }
+
+    if (version) {
+      this.versionPath = this.restAPI.root.addResource(version);
+    }
   }
 }
--- src/types/APIGateway.type.ts
+++ src/types/APIGateway.type.ts
@@ -1,14 +1,18 @@
 import { z as zod } from 'zod';
-import { RestApiProps } from 'aws-cdk-lib/aws-apigateway';
+import { IDomainName, RestApiProps } from 'aws-cdk-lib/aws-apigateway';
 import { ApiGatewayProps as CdkApiGatewayProps } from 'aws-cdk-lib/aws-events-targets';
 
 export const ExtendedApiGatewayPropsSchema = zod.object({
   serviceName: zod.string(),
   env: zod.string(),
+  customDomainName: zod.string().optional(),
+  version: zod.string().optional(),
   rateLimit: zod.number().optional(),
   burstLimit: zod.number().optional(),
   apiKeyRequired: zod.boolean().optional(),
   restApiProps: zod.object({}).optional(),
+  domainNameAliasTarget: zod.string().optional(),
+  domainNameAliasHostedZoneId: zod.string().optional(),
 });
 
 export interface ApiGatewayProps
